356795e1fb4a0b239828d1f92374624c959b5569
hamed <hamedmoradi.mailsbox@gmail.com>
2025-03-02 01:39:19 +0330

    optimize imports

--- src/main/java/com/example/kiantask/pattern/observer/TransactionObserver.java
+++ src/main/java/com/example/kiantask/pattern/observer/TransactionObserver.java
@@ -1,7 +1,5 @@
 package com.example.kiantask.pattern.observer;
 
-import java.io.IOException;
-
 public interface TransactionObserver {
     void onTransaction(String accountNumber, String transactionType, double amount);
 }
